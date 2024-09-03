0729591fc47631efd1a8cd5361440572f822cd78
Milena Lara <milenalrf@gmail.com>
2024-09-02 22:10:58 -0300

    Created projects source code from spring initializr

---
+++ project/code/aluguelDeCarros/src/main/java/br/pucminas/aluguelDeCarros/AluguelDeCarrosApplication.java
@@ -0,0 +1,13 @@
+package br.pucminas.aluguelDeCarros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluguelDeCarrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluguelDeCarrosApplication.class, args);
+	}
+
+}
---
+++ project/code/aluguelDeCarros/src/test/java/br/pucminas/aluguelDeCarros/AluguelDeCarrosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.pucminas.aluguelDeCarros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluguelDeCarrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
